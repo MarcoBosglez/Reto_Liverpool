fa095eb38f3aba71f63af2889bec6f122be2a626
Marco Bosquez <mark.bosglez@gmail.com>
2021-04-21 20:38:10 -0500

    Update settings.py

--- retoLiverpool/settings.py
+++ retoLiverpool/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'retoLiverpool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
@@ -86,7 +87,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
