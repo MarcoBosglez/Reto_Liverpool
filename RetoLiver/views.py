d078ff3ed505e7014f294656a212ebf2619a87d7
Marco Bosquez <mark.bosglez@gmail.com>
2021-04-30 16:24:43 -0500

    HTMLS

--- RetoLiver/views.py
+++ RetoLiver/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def proceso(request):
+    telefono = request.POST['phone']
+    correo = request.POST['email']
+    card = request.POST['card']
+    return render(request, 'indexC.html', {'Phone': telefono, 'Email': correo, 'Check': card})
